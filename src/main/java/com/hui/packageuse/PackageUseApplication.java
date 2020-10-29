120075afb0e97575f45c8802f304d419813f661a
luma <liuqinghui1991@gmail.com>
2020-10-29 23:36:15 +0800

    update

---
+++ src/main/java/com/hui/packageuse/PackageUseApplication.java
@@ -0,0 +1,13 @@
+package com.hui.packageuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageUseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackageUseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hui/packageuse/PackageUseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hui.packageuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageUseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
